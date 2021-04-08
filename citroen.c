6f019eb6cda5ec831131a50c8c871f1772381d3e
jmm98sr <60983135+jmm98sr@users.noreply.github.com>
2021-04-08 20:30:51 +0200

    comments to english

--- citroen.c
+++ citroen.c
@@ -3,15 +3,15 @@
 
 char* citroenTPMS(char* state, char *id, int flags, int repeat, float pressure, float temperature, int battery/*, char *filename*/){
 
-    /*Preambulo: parte inicial de trama, indica el inicio de la comunicacion*/
+    /*Preamble: initial part of frame, indicates the start of communication*/
     char* preamble = "01010101010101010101010101010110";
 
-    /*finaltrail: parte final de trama, indica el final de la comunicacion*/
+    /*finaltrail: end part of frame, indicates the end of the communication*/
     char* finaltrail = "01111110";
 
-    /*Trama a codificar*/
-        /*conversion de datos a binario*/
-    char* systemState = dec2bin((long int)strtol(state, 0, 16), 8);/*con strtol convertimos de hexadecimal a decimal*/
+    /*Frame to encode*/
+        /*data conversion to binary*/
+    char* systemState = dec2bin((long int)strtol(state, 0, 16), 8);/*with strtol we convert from hexadecimal to decimal*/
     char* systemID = dec2bin((long int)strtol(id, 0, 16), 32);
     char* systemFlags = dec2bin(flags, 4);
     char* systemRepeat = dec2bin(repeat, 4);
@@ -30,9 +30,9 @@ char* citroenTPMS(char* state, char *id, int flags, int repeat, float pressure, 
     strcat(frame, systemBattery);
     
 
-    /*Conversion de caracter a digito*/
+    /*Character to digit conversion*/
     
-    /*Calculo del checksum mediante la operacion xor entre elementos de la trama separados en grupos de 8 bits*/
+    /*Calculation of the checksum by means of the xor operation between elements of the frame separated into groups of 8 bits*/
     char* checksum = (char*)malloc(8);
     strncpy(checksum, frame, 8);
 
@@ -49,24 +49,24 @@ char* citroenTPMS(char* state, char *id, int flags, int repeat, float pressure, 
         j++;
     }  
 
-    /*Construccion de la trama final que deberemos modular*/
+    /*Construction of the final frame that we will have to modulate*/
     char* full_frame = (char*)malloc(80);  
     strcpy(full_frame, frame);
     
     strcat(full_frame, checksum);
     printf("%s %s\n", "Trama", full_frame);
 
-    /*Codificacion de la señal en manchester*/
+    /*Signal encoding in manchester*/
     char* manchester_frame = manchester_encoder(full_frame);
     printf("%s %s\n", "Trama Manchester", manchester_frame);
 
-    /*Construccion de la trama codificada. El preambulo y el fin de la trama no se codifican*/
+    /*Construction of the coded frame. The preamble and the end of the frame are not encoded*/
     char* finalCodifiedFrame = (char*)malloc(120);/*duda con el tamaño del preambulo*/
     strcpy(finalCodifiedFrame, preamble);
     strcat(finalCodifiedFrame, manchester_frame);
     strcat(finalCodifiedFrame, finaltrail);
 
-    /*Escritura o devolucion de la señal*/
+    /*Write or return the signal*/
     printf("%s\n%s\n", "Trama final",finalCodifiedFrame);
     printf("%s\n%ld\n", "Tamaño total",strlen(finalCodifiedFrame));    
 
